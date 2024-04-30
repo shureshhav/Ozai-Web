ca4bc3d77a04b9b18394d3eff8f7c5096582348d
Nagaraju Chilakalapudi <47734826+cpdnagaraju@users.noreply.github.com>
2024-04-30 11:22:09 +0530

    Add files via upload

---
+++ WEB-INF/classes/com/ozai/model/Badges.java
@@ -0,0 +1,65 @@
+package com.ozai.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.ozai.util.OzaiUtil;
+
+@Entity
+@Table(name = "BADGES")
+public class Badges implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	public Badges() {
+		super();
+	}
+	
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "title")
+	private String title;
+	
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}	
+	
+}
